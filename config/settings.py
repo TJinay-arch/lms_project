7b3eb5cdb04090041ae09a02ded3015a20c0ce7b
Ilya Payusov <ilya-payusov@mail.ru>
2026-04-20 22:11:43 +0300

    changed allowed hosts 2

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['81.26.179.6']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
